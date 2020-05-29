db27d01a594a90ebee361d28a665e30f0d8d5f2c
Avi <avi@localhost.localdomain>
2020-05-29 15:50:13 +0530

    cleaning the landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,13 @@ import React from "react"
 
 import Layout from "../components/layout"
 import Head from "../components/head"
-import dcav from "./dcav.png"
 
 const IndexPage = () => {
   return (
     <Layout>
       <Head title="Home" />
-      <img src={dcav} alt="Design Code Analize Visualize" />
+      <h1>Code Design Analize Visualize</h1>
+      {/* <img src={dcav} alt="Design Code Analize Visualize" /> */}
       <p>{`explore the site :)`}</p>
     </Layout>
   )
