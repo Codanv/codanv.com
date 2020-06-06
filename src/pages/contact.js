8e7977a5fd723a2e71be07278a50c8fe7e6edd1a
Avi <avi@localhost.localdomain>
2020-06-06 15:30:03 +0530

    removed contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,26 +8,9 @@ const ContactPage = () => {
     <Layout>
       <Head title="Contact" />
       <h1>Get in touch</h1>
-      <p>
+      <p>DM →
         <a href="https://twitter.com/codanv" target="_blank" rel="noopener noreferrer">Twitter</a>     
       </p>
-      <h2>Your concern</h2>
-      <form name="contact" method="post" netlify-honeypot="bot-field" data-netlify="true">
-        <input type="hidden" name="bot-field" />
-        <input type="hidden" name="form-name" value="contact" />
-        <p>
-          <label>Your Name: <input type="text" name="name"/></label>
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email"/></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
     </Layout>
   )
 }
