4a80eea78afaa2849a96605165bd0bf103469f1c
Avi <avi@localhost.localdomain>
2020-06-07 21:03:08 +0530

    fixed landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,16 +10,17 @@ const IndexPage = () => {
     <Layout>
       <Head title="Home" />
       <div className={indexStyles.center}>
-        <h1>Model real life entities by designing, coding, analysing and    visualizing.
+        <h1>Model real life entities by designing, coding, analysing and    visualizing
         </h1>
-        <p>In order to do so, we need to have the right mindset and specific   domain knowledge.
+        <p>In order to do so, we need to have the right mindset and specific   domain knowledge
         </p>
-        <p> Let's continue the endeavour...</p>
-        <p> explore{" "} 
+        
+        <p>
           <span className={indexStyles.blogLink}>
-            <Link to="/blog">Blog →</Link>
+            <Link to="/blog">Get Started →</Link>
           </span>
         </p>
+        <p> Let's continue the endeavour...</p>
       </div>
     </Layout>
   )
