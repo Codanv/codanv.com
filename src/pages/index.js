ca0fac289951e2bb74bfc9208c14d89411ca7f54
Avi <avi@localhost.localdomain>
2020-07-13 12:35:34 +0530

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ const IndexPage = ({
             Design Code Analyze Visualize</h1>
             <a href="https://tinyletter.com/codanv" target="_blank" rel="noreferrer noopener" style={{border: `1px solid rgba(0, 0, 0, 0.2)`, padding: `0.5rem 1rem`, borderRadius: `0.2rem`, boxShadow: `0.2rem 0.2rem 0.3rem #000000`, marginRight: `1rem`}}>SUBSCRIBE</a>
 
-            <Link to="/posts" style={{border: `1px solid rgba(0, 0, 0, 0.2)`, padding: `0.5rem 1rem`, borderRadius: `0.2rem`, boxShadow: `0.2rem 0.2rem 0.3rem #000000`}}>Explore</Link>
+            <Link to="/posts" style={{border: `1px solid rgba(0, 0, 0, 0.2)`, padding: `0.5rem 1rem`, borderRadius: `0.2rem`, boxShadow: `0.2rem 0.2rem 0.3rem #000000`}}>EXPLORE</Link>
           </div>
 
           <div className={indexStyles.section}>
