622cb657ff6592f0b8280a3067fcfd61c1fd1176
Avi <avi@localhost.localdomain>
2020-07-20 20:49:22 +0530

    added podcasts list

---
+++ src/pages/podcasts.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from "../components/layout"
+import Head from "../components/head"
+
+const Podcasts = () => {
+    return (
+        <>
+            <Head  title="Podcasts" canonical="https://www.codanv.com/podcasts/" />
+            <Layout>
+                <h1>Podcasts</h1>
+                <p>list is to be updated here</p>
+            </Layout>
+        </>
+    )
+}
+
+export default Podcasts
