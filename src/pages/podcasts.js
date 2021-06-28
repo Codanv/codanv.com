af522dd533d78c19524c018dc2a02fc682cb81f7
codanv <codanv@codanv.com>
2021-06-28 13:11:09 +0530

    podcast updated

--- src/pages/podcasts.js
+++ src/pages/podcasts.js
@@ -9,18 +9,19 @@ const Podcasts = () => {
             <Layout>
                 <h1>Podcasts</h1>
                 <p>
-                    <h2>Open Source - Series Introduction</h2>
+                    <a 
+                        href="https://anchor.fm/codanv/episodes/S01E01---Open-Source---Series-Introduction--Codanv-e13ih93"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                    <h3>S01E01 - Open Source - Series Introduction</h3>
+                   </a>
+                   
                     This series is dedicated to right from contributing, creating to maintaining open source projects in details.
 
                     In this episode there is a list of questions which you can expect to be answered in this series. This is equally beneficial for beginners and experienced learners.
 
-                    So do listen and share with your friends. keep learning!
-                <a 
-                    href="https://anchor.fm/codanv/episodes/S01E01---Open-Source---Series-Introduction--Codanv-e13ih93"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >S01E01 - Open Source - Series Introduction</a>
-                    
+                    So do listen and share with your friends. keep learning! 
                 </p>
             </Layout>
         </>
