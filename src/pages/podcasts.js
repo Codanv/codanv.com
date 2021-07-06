f3b76e662df23c72c15cbbc2cabd8352ec3188ae
codanv <codanv@codanv.com>
2021-07-06 08:06:10 +0530

    updated

--- src/pages/podcasts.js
+++ src/pages/podcasts.js
@@ -8,125 +8,19 @@ const Podcasts = () => {
             <Head  title="Podcasts" canonical="https://www.codanv.com/podcasts/" />
             <Layout>
                 <h1>Podcasts <small><a href="https://anchor.fm/s/2cd2ed40/podcast/rss">RSS</a></small></h1>
-                <p>
-                    <a 
-                        href="https://open.spotify.com/show/7DKSqNSYfUniEeqOqNOdFj"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                    <h3>S01E01 - Open Source - Series Introduction</h3>
-                   </a>
-                    <p>
-                        <iframe src="https://anchor.fm/codanv/embed/episodes/S01E01---Open-Source---Series-Introduction--Codanv-e13ih93" height="102px" width="400px" frameborder="0" scrolling="no"></iframe>
-                    </p>
-                    This series is dedicated to right from contributing, creating to maintaining open source projects in details.
 
-                    In this episode there is a list of questions which you can expect to be answered in this series. This is equally beneficial for beginners and experienced learners.
-                </p>
+                <p>Available on several platforms</p>
 
-                <p>
-                    <a
-                        href="https://open.spotify.com/show/7DKSqNSYfUniEeqOqNOdFj"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h3>S01E02 - Open Source - What and Why</h3>
-                    </a>
-                    <p>
-                        <iframe src="https://anchor.fm/codanv/embed/episodes/S01E02---Open-Source---What-and-Why-e13k9do" height="102px" width="400px" frameborder="0" scrolling="no"></iframe>
-                    </p>
-                    Welcome to `What and Why` episode of `Open Source` series. There are three main concerns discussed in this episode -
-                    <ul>
-                        <li>What does mean to be a open source project?</li>
-                        <li>Is every open source project free of charge?</li>
-                        <li>Why does people opensource their project or work?</li>
-                    </ul>  
-                </p>
+                <a href="https://open.spotify.com/show/7DKSqNSYfUniEeqOqNOdFj" target="_blank" rel="noopener noreferrer">Spotify</a>
 
-                <p>
-                    <a
-                        href="https://open.spotify.com/show/7DKSqNSYfUniEeqOqNOdFj"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h3>S01E03 - Open Source - Objectives &amp; Meaning of Contributing for a beginner</h3>
-                    </a>
-                    <p>
-                    <iframe src="https://anchor.fm/codanv/embed/episodes/S01E03---Open-Source---Objectives--Meaning-of-Contributing-for-a-Beginner-Hindi-e13m3d1" height="102px" width="400px" frameborder="0" scrolling="no"></iframe>
-                    </p>
-                    Hello friends! There are two concerns tackled in this episode:  
-                    <ul>
-                        <li>1. Probable objectives of contributing.</li>
-                        <li>2. Initially, How can a beginner start contributing without involving in coding?</li>
-                    </ul>  
-                </p>
+                <a href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy8yY2QyZWQ0MC9wb2RjYXN0L3Jzcw==" target="_blank" rel="noopener noreferrer">Google Podcast</a>
 
-                <p>
-                    <a
-                        href="https://open.spotify.com/episode/7dWkj19PwBDawYQE6C9fyh"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h3>E01S04 - Open Source - Anatomy &amp; Choosing a Project to Contribute</h3>
-                    </a>
-                    <p>
-                        <iframe src="https://anchor.fm/codanv/embed/episodes/E01S04---Open-Source---Anatomy--Choosing-a-Project-to-Contribute-e13o5hr" height="102px" width="400px" frameborder="0" scrolling="no"></iframe>
-                    </p>
-                    Welcome Friends! We'll discuss - 1. Internal workings of an open source project and 2. how to choose a project to contribute. 
-                </p>
+                <a href="https://www.breaker.audio/codanv" target="_blank" rel="noopener noreferrer">Breaker</a>
 
-                <p>
-                    <a
-                        href="https://open.spotify.com/episode/3Z2nNOQs8NYhL8zzSTeIk8"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h3> S01E05 - Open Source - Submitting Contribution &amp; After Contribution</h3>
-                    </a>
-                    <p>
-                        <iframe src="https://anchor.fm/codanv/embed/episodes/S01E05---Open-Source---Submitting-Contributing--After-Contribution-Hindi-e13q1l8" height="102px" width="400px" frameborder="0" scrolling="no"></iframe>
-                       
-                    </p>
-                    Hello Doston! Concerns of this episode are:
-                    <ul>
-                        <li>How to ensure effective communication in open source project community</li>
-                        <li>How to open an issue or pull request effectively</li>
-                        <li>Steps needed to open a pull request</li>
-                        <li>What if you don't get any response after contributing or even rejected </li>
-                    </ul>
-                </p>
+                <a href="https://radiopublic.com/codanv-WxBxb7" target="_blank" rel="noopener noreferrer">Radio Public</a>
 
-                <p>
-                    <a
-                        href="https://open.spotify.com/episode/3JrxPUVxlIyidx75XDSaLV"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h3>S01E06 - Open Source - How to Launch a Project</h3>
-                    </a>
-                    <p>
-                        <iframe src="https://anchor.fm/codanv/embed/episodes/S01E06---Open-Source---How-to-Launch-a-Project-Hindi-e13rn6v" height="102px" width="400px" frameborder="0" scrolling="no"></iframe>
-                    </p>
-                        Hello Friends! This episode explains
-                        <ul>
-                        <li>Documentations guidelines  about LICENSE, README, CONTRIBUTING, CODE_OF_CONDUCT </li>
-                        <li>Branding</li>
-                        </ul>
-                </p>
+                <a href="https://anchor.fm/codanv" target="_blank" rel="noopener noreferrer">Anchor</a>
 
-                <p>
-                    <a
-                        href="https://open.spotify.com/episode/2Fk9GV6ISeHVv4V5OPxiLb"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h3>S01E07 - Open Source - Finding Users for a Project</h3>
-                    </a>
-                    <p>
-                        <iframe src="https://anchor.fm/codanv/embed/episodes/S01E07---Open-Source---Finding-Users-for-a-Project-Hindi-e13suje" height="102px" width="400px" frameborder="0" scrolling="no"></iframe>
-                    </p>
-                        Hello Doston! In every open source project, People often involve as a user eventually turns in contributors. So we need to find users for our project. This is what the concern of this episode.
-                </p>
             </Layout>
         </>
     )
